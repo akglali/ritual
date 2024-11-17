fcd3739f1a38e775e45cb8aabfc2b1a145d46852
Ali <47037725+akglali@users.noreply.github.com>
2024-11-17 11:45:26 -0500

    Update ritual_node_run.sh

--- ritual_node_run.sh
+++ ritual_node_run.sh
@@ -119,7 +119,12 @@ off_chain() {
 on_chain() {
     echo "Running On-chain tasks..."
 
-  
+    echo "Installing Forge dependencies..."
+    # Ensure you're in the correct directory
+    cd ~/infernet-container-starter/projects/hello-world/contracts || { echo "Error: Directory not found."; exit 1; }
+    forge install --no-commit foundry-rs/forge-std || { echo "Error: Forge command failed."; exit 1; }
+    forge install --no-commit ritual-net/infernet-sdk || { echo "Error: Forge command failed."; exit 1; }
+    cd ../../../
    
     echo "*******************************"
     echo "Deploying contracts for project hello-world..."
